0b72209d65999253212f10041333719d3d848d92
Sara Askari Hemmat <51385797+sara24h@users.noreply.github.com>
2025-12-18 21:49:29 +0330

    Update __init__.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,3 +1,11 @@
-from .seed import set_seed, worker_init_fn
+# dynamic_hesitant/utils/__init__.py
 
-__all__ = ['set_seed', 'worker_init_fn']
+"""
+This file makes the 'utils' directory a Python package.
+It imports the necessary functions from utils.py to make them available
+when importing from the 'utils' package.
+"""
+
+from .utils import set_seed, setup_ddp, cleanup_ddp
+
+__all__ = ['set_seed', 'setup_ddp', 'cleanup_ddp']
