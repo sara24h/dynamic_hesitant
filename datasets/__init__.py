e4f85f01245559447cba1235a3869baea3955ea1
Sara Askari Hemmat <51385797+sara24h@users.noreply.github.com>
2025-12-18 21:37:37 +0330

    Update __init__.py

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -1,10 +1,5 @@
 # dynamic_hesitant/datasets/__init__.py
 
-"""
-This file serves as the entry point for the 'datasets' package.
-It exposes the main function `create_dataloaders_ddp` to other parts of the project.
-"""
-
 from .base_dataset import create_dataloaders_ddp
 
 __all__ = ['create_dataloaders_ddp']
