269494a91920d173bf2a1d9d44820001306e5de5
Sara Askari Hemmat <51385797+sara24h@users.noreply.github.com>
2026-02-10 19:08:13 +0330

    Update stacking.py

--- stacking.py
+++ stacking.py
@@ -370,7 +370,7 @@ def main():
     parser.add_argument('--num_grad_cam_samples', type=int, default=5)
     parser.add_argument('--num_lime_samples', type=int, default=5)
     parser.add_argument('--dataset', type=str, required=True,
-                       choices=['wild', 'real_fake', 'hard_fake_real', 'custom_genai', 'uadfV','real_fake_dataset'])
+                       choices=['wild', 'real_fake', 'hard_fake_real', 'custom_genai', 'custom_genai_v2', 'uadfV', 'real_fake_dataset'])    
     parser.add_argument('--data_dir', type=str, required=True)
     parser.add_argument('--model_paths', type=str, nargs='+', required=True)
     parser.add_argument('--model_names', type=str, nargs='+', required=True)
