0959fb60c5c3f4d8112b15c2c55044ff97340639
Sara Askari Hemmat <51385797+sara24h@users.noreply.github.com>
2026-02-16 13:46:50 +0330

    Update stacking.py

--- stacking.py
+++ stacking.py
@@ -40,8 +40,8 @@ def set_seed(seed: int = 42):
     torch.manual_seed(seed)
     torch.cuda.manual_seed_all(seed)
     # برای حداکثر سرعت، deterministic را خاموش و benchmark را روشن می‌کنیم
-    torch.backends.cudnn.deterministic = False
-    torch.backends.cudnn.benchmark = True
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
     os.environ['PYTHONHASHSEED'] = str(seed)
 
 
